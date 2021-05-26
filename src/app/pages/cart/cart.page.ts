c69cdac8f44badd72575448a8dce1b2a1a34e3e2
suhaibh27 <40864369+suhaibh27@users.noreply.github.com>
2021-05-26 16:02:30 +0300

    cart

--- src/app/pages/cart/cart.page.ts
+++ src/app/pages/cart/cart.page.ts
@@ -13,6 +13,7 @@ import { FirestoreService } from '../../provider/firestore.service';
 export class CartPage implements OnInit {
   cart=[];
   products=[];
+  empty=false;
   constructor(private loadingController: LoadingController ,private firestore: FirestoreService) {
 
    }
@@ -30,13 +31,16 @@ export class CartPage implements OnInit {
         }
     });
     this.loadingController.dismiss();
-    });
+    if(this.cart.length<=0){this.empty=true;}});
   }
 
   checkSameItem(elem){
-    for(let it of this.cart){
-      if(it.itemID==elem.itemID){
-        it.quantity+=elem.quantity;
+    for(let i=0;i<this.cart.length;i++){
+      if(this.cart[i].itemID==elem.itemID){
+        this.cart[i].quantity+=elem.quantity;
+        if(this.products[i]&&this.products[i].quantity<this.cart[i].quantity){
+          this.cart[i].quantity=this.products[i].quantity;
+        }
         return true;
       }
     return false;
@@ -62,6 +66,18 @@ export class CartPage implements OnInit {
 
       const { role, data } = await loading.onDidDismiss();
       console.log('Loading dismissed!');
+    }
+    delete(i){
+      if(i==0){
+        this.products.splice(i,i+1);
+        this.cart.splice(i,i+1);
+      }
+      else{
+        this.products.splice(i,i);
+        this.cart.splice(i,i);
+        console.log(this.cart[i]+''+this.products[i])
+      }
+      if(this.cart.length<=0){this.empty=true;}
     }
 
 }
